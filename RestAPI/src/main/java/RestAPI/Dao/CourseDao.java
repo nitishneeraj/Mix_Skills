ca35f756ac914ceca5358c4417476ef7d972636d
Nitish Kumar <system@CKS.localdomain>
2022-08-04 16:14:33 +0530

    Change file location

--- RestAPI/src/main/java/RestAPI/Dao/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package RestAPI.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import RestAPI.Controller.entities.Course;
-
-public interface CourseDao extends JpaRepository<Course, Long> {
-
-}
